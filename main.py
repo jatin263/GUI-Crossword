787a2d4bed00f318db7f8396309e6ca9a0bef3d7
Jatin Kumawat <jatinkumawat20@gmail.com>
2022-10-09 12:34:42 +0530

    Layout Done

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+root = Tk()
+root.geometry("1000x1000")
+button = Button(root, text='bbb', bg='black' ,state=DISABLED)
+button.grid(row=0, column=0)
+button1 = Button(root, text='bbb', bg='black' ,state=DISABLED)
+button1.grid(row=1, column=0)
+e1 = Entry(root,width=2,font=2)
+e1.grid(row = 2, column = 1)
+root.mainloop()
