5cdc03ced217aa1a0e9cbdece89d825ad18c3612
Jatin Kumawat <jatinkumawat20@gmail.com>
2022-10-10 12:12:45 +0530

    Grid done

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+from cProfile import label
 from tkinter import *
-def go():
-    text = e1.get()[0]  
-    e1.delete(0, END)  
-    e1.insert(0, text)
+def callback(var):
+   content= var.get()[0]
+   if content.isalpha():
+      e1.delete(0, END)   
+      e1.insert(0, content)
+   else:
+      e1.delete(0,END)
 root = Tk()
 root.geometry("1000x1000")
+root.config(bg="blue")
+user_name = Label(root,text = "Username",font=20,width=20,height=5).grid(row =0,column=20) 
 button = Button(root, text='bbb', bg='black' ,state=DISABLED)
-button.grid(row=0, column=0)
+button.grid(row=1, column=0)
 button1 = Button(root, text='bbb', bg='black' ,state=DISABLED)
-button1.grid(row=1, column=0)
+button1.grid(row=2, column=0)
 button2 = Button(root, text='bbb', bg='black' ,state=DISABLED)
 button2.grid(row=4, column=7)
-e1 = Entry(root,width=2,font=2)
-e1.grid(row = 2, column = 1)
+var = StringVar()
+var.trace("w", lambda name, index,mode, var=var: callback(var))
+#Create an Entry widget
+e1 = Entry(root, textvariable=var,width=2,font=2)
+e1.grid(row=2,column=1)
 root.mainloop()
--- t.py
+++ t.py
@@ -6,9 +6,13 @@ win= Tk()
 win.geometry("750x250")
 def callback(var):
    content= var.get()[0]
-   e.delete(0, END)   #deletes everything
-   e.insert(0, content)
-   print(content)
+   if content.isalpha():
+      e.delete(0, END)   
+      e.insert(0, content)
+      print(content)
+   else:
+      e.delete(0,END)
+
 #Create an variable to store the user-input
 var = StringVar()
 var.trace("w", lambda name, index,mode, var=var: callback(var))
